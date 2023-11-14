9dee0ffbbdf468b58bf1ffc0d7e72bb30d480a68
Keran Barbieri <keranbarbieri@hotmail.com>
2023-11-14 08:26:48 -0300

    Project delivery

--- classificadorHeroi.js
+++ classificadorHeroi.js
@@ -1,7 +1,36 @@
-const nomeHeroi = "Keran";
 
-let xp = 50;
+let nomeDoHeroi = "Keran"; 
+let experienciaDoHeroi = 19000;
 
-let nivel = "";
+let nivelDoHeroi;
 
+//o switch é usado com a expressão true para avaliar as condições em cada case.
+switch (true) {
+    case experienciaDoHeroi < 1000:
+        nivelDoHeroi = "Ferro";
+        break;
+    case experienciaDoHeroi >= 1001 && experienciaDoHeroi <= 2000:
+        nivelDoHeroi = "Bronze";
+        break;
+    case experienciaDoHeroi >= 2001 && experienciaDoHeroi <= 5000:
+        nivelDoHeroi = "Prata";
+        break;
+    case experienciaDoHeroi >= 6001 && experienciaDoHeroi <= 7000:
+        nivelDoHeroi = "Ouro";
+        break;
+    case experienciaDoHeroi >= 7001 && experienciaDoHeroi <= 8000:
+        nivelDoHeroi = "Platina";
+        break;
+    case experienciaDoHeroi >= 8001 && experienciaDoHeroi <= 9000:
+        nivelDoHeroi = "Ascendente";
+        break;
+    case experienciaDoHeroi >= 9001 && experienciaDoHeroi <= 10000:
+        nivelDoHeroi = "Imortal";
+        break;
+    default:
+        nivelDoHeroi = "Radiante";
+}
+
+
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}`);
 
